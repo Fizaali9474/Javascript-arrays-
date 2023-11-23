14cd75cdb15fab76963a0d13d0a019f9353dfd57
Fiza Ali <140485687+Fizaali9474@users.noreply.github.com>
2023-11-23 07:02:14 -0800

    Add files via upload

---
+++ JS (arrays)/app.js
@@ -0,0 +1,161 @@
+/*
+var arr =['warid','jazz','ufone','telenor']
+
+document.write(arr)
+
+
+var arr = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"]
+document.write(arr)
+
+
+var arr =['Movies2015']
+
+Movies2015.push("fiza")
+Movies2015.push("fiza")
+Movies2015.push("fiza")
+Movies2015.push("fiza")
+
+
+document.write(" Movies2015:")
+
+document.write("Length of the array: " + Movies2015.length)
+
+
+
+var cars = ["Tesla Model S", "Porsche 911", "BMW M3", "Audi R8"];
+
+var firstindex = 0;
+
+
+var caratfirstindex = cars[firstindex];
+
+
+var lastindex = cars.length - 1
+var caratlastindex = cars[lastindex]
+
+
+document.write("a. First index of the array: " + firstindex)
+document.write("b. Car at first index of the array: " + caratfirstindex)
+document.write("c. Last index of the array: " + lastindex);
+document.write("d. Car at last index of the array: " + caratlastindex)
+
+
+var studentnames = ["fiza", "ali", "saim"]
+var studentscores = [200, 300, 400]
+
+
+function calculatePercentage(score, totalMarks) {
+    return ((score / totalMarks) * 100).toFixed(0)
+}
+
+
+var totalMarks = 500
+
+for (var i = 0; i < studentnames.length; i++) {
+    var name = studentnames[i]
+    var score = studentscores[i]
+    var percentage = calculatePercentage(score, totalMarks)
+
+    document.write("Score of " + name + " is " + score + ". Percentage: " + percentage + "%")}
+
+
+var colorArray = ["Red", "Green", "Blue"];
+
+document.write("Original Array: " + colorArray);
+
+
+var colorToAddAtStart = prompt("Enter a color to add to the beginning:");
+colorArray.unshift(colorToAddAtStart);
+
+var colorToAddAtEnd = prompt("Enter a color to add to the end:");
+colorArray.push(colorToAddAtEnd);
+
+
+colorArray.unshift("Purple", "Yellow")
+
+colorArray.shift();
+
+
+colorArray.pop();
+
+
+var indexToAdd = prompt("Enter the index to add a color:");
+var colorToAddAtIndex = prompt("Enter the color name to add at the index:");
+colorArray.splice(indexToAdd, 0, colorToAddAtIndex);
+
+
+var indexToRemove = prompt("Enter the index to start removing colors:");
+var numberOfColorsToRemove = prompt("Enter the number of colors to remove:");
+colorArray.splice(indexToRemove, numberOfColorsToRemove);
+
+
+document.write("Updated Array: " + colorArray)
+
+
+
+var studentscores = [320, 230, 480, 120];
+
+
+document.write("Scores of Students: " + studentscores);
+
+
+
+
+document.write("Ordered Scores of Students: " + studentScores)
+
+
+
+var fruits = ["strawberry", "apple", "orange", "banana"];
+
+
+document.write("fruits list:" )
+
+
+
+var obj ={
+arr :[1 , 2, 3 , 4 , 5 , 6]
+} 
+
+for (var key in obj)
+{
+    document.write(key, '<br>')
+}
+
+
+
+var arroFObj=[
+
+{
+
+name:'fiza',
+age:'17',
+des:'hello world',
+text:'abc',
+
+},
+
+{
+
+    name:'fiza',
+    age:'17',
+    des:'hello world',
+    text:'abc',
+    
+    },
+
+    {
+
+        name:'fiza',
+        age:'17',
+        des:'hello world',
+        text:'abc',
+        
+        }
+
+
+]*/
+
+
+
+
+
